e4fa5114ed272c8524a81af5ecdb748af8d9c561
Ubuntu <dbotan@Ubuntu-x64-Europe.gqmmixtt1x2enad0igew5bi2gh.ax.internal.cloudapp.net>
2018-10-31 22:27:35 +0000

    Get data from Shibe API

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ const fs = require('fs');
 const http = require('http');
 const port = 8080;
 
-const getShiba = () => {
-}
-
 const requestHandler = (request, response) => {
+	const getShiba = () => {
+		http.get('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true', (resp) => {
+			let data = '';
+
+			resp.on('data', (chunk) => {
+				data += chunk;
+			});
+
+			resp.on('end', () => {
+				response.end(JSON.parse(data)[0]);
+			});
+		}).on('error', (err) => {
+			console.log('Error:', err.message);
+		});
+	}
+
 	console.log(request.url);
 
 	if(request.url == '/')
 		response.end(fs.readFileSync('index.html', {encoding: 'utf-8'}));
 	else if(request.url == '/shiba')
-		response.end('https://cdn.shibe.online/shibes/3c605814d42001602ca31d718b663ed000daaba0.jpg');
+		response.end(getShiba());
 	else
 		response.end(fs.readFileSync(request.url.substr(1)));
 }
@@ -20,7 +33,7 @@ const requestHandler = (request, response) => {
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
-	if(err) return console.log('Error:', err);
+	if(err) return console.log('Error:', err.message);
 
 	console.log('Server is listening on', port);
-})
+});
